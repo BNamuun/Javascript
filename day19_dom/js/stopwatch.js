eb16b95d1ec7c4cffe7be2775996ab4833b914e0
Namuun_L <mit.irnitu@gmail.com>
2022-12-16 14:45:15 +0800

    stopwatch js

--- day19_dom/js/stopwatch.js
+++ day19_dom/js/stopwatch.js
@@ -1,3 +1,14 @@
-const startBtn = document.getElementsByClassName('start')
-const stopBtn = document.getElementsByClassName('stop')
-const resetBtn = document.getElementsByClassName('reset') 
+// let hr = min = sec = ms= 0,
+// startTimer;
+// function startButton(){
+//     document.getElementById('ex').style.opacity = '0.8';
+//     startTimer = setInterval(() =>{
+//         ms++
+//     }, 100) 
+//     let hourBtn = document.getElementsByClassName('HourBtn')[0].innerHTML;
+//     hourBtn =ms
+// }
+
+const startBtn = document.querySelector(".start")
+const stopBtn = document.querySelector(".stop")
+const resetBtn = document.querySelector(".reset")
