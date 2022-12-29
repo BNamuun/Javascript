6d503c2e82afef6498e85876919f61d21cce6dba
Evening <evening@admins-iMac.local>
2022-12-29 17:40:57 +0800

    product list

--- HW/Product_list/productList.js
+++ HW/Product_list/productList.js
@@ -1,21 +1,32 @@
 // link to API typicon
+
 fetch('https://dummyjson.com/products')
 .then((resp) => resp.json())
 .then((data) =>{
     const dataList = data.products;
-    console.log(dataList);
+    // console.log(dataList);
     const brandinf = dataList.map(item =>
-        `<div class = 'briedInfo col-lg-12'>
+        `<div class = 'briefInfo p-5 mb-5 col-xl-3 col-lg-4 col-md-6 col-sm-8 col-12'>
+        <div class = "descr">
            <h2> ${item.brand} </h2>
            <p> ${item.description} </p>
-           <img src ="${item.images[0]}"
+           </div>
+           <div class = "imageDiv">
+                     <img class = "img-feature" src ="${item.images[0]}"
+         
            <div class = "price d-flex">
-           <h6>${item.price}</h6>
-           <h6> ${item.discountPercentage}</h6>
+           <h6> Price: $ ${item.price}</h6>
+           <h6 class = "disc"> Discount:  ${item.discountPercentage} %</h6>
+           <h5 class = "btn btn-primary text-white"> More </h5>
+            </div>
             </div>
         </div>`
     ).join(" ")
-    console.log(brandinf);
+    // console.log(brandinf);
     document.getElementById('productList').innerHTML = brandinf
-    
 })
+
+
+
+const getValue = document.getElementsByClassName('inputField').Value
+console.log(getValue);
