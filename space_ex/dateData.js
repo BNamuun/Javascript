79b69fb51844becbba4063f5109c7be6de408e0c
Evening <evening@admins-iMac.local>
2023-01-03 17:31:58 +0800

    chart diagram time

--- space_ex/dateData.js
+++ space_ex/dateData.js
@@ -1,52 +1,75 @@
-indexVal = []
-const DateTime = []
-const dateP = []
-const Po =[]
-const TimeP = []
+indexVal = [];
+const DateTime = [];
+const dateP = [];
+const TimeArr = [];
+const TimeP = [];
 const loadCharacters = async () => {
-    try {
-        const res = await fetch('https://services.swpc.noaa.gov/products/noaa-planetary-k-index.json#')
-        let data = await res.json();
-        // console.log(data);
-        data.map(item =>
-        // cosnt DateVal =  DateTime.push((item)[0])
-        indexVal.push((item)[1]))
-        indexVal.shift();
-        console.log({indexVal})
-        const ctx = document.getElementById('myChart');
-        
-        data.map(item1 =>
-            // cosnt DateVal =  DateTime.push((item)[0])
-            DateTime.push((item1)[0]))
-            DateTime.shift();
-            DateTime.slice(11,15);
-            // Po.push(new Date(dateP));
-            // dateP.push(DateTime.getHours() + ':' + DateTime.getMinutes());
-            
-            new Chart(ctx, {
-                type: 'bar',
-            data: {
-                labels: DateTime.slice(-10),
-                datasets: [{
-                    label: 'Kp index',
-                    data: indexVal.slice(-10),
-                    borderWidth: 2
-                }]
-            },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
-                }
-            }
-        });
-        
-    } catch (err) {
-        console.log(err);
+  try {
+    const res = await fetch(
+      "https://services.swpc.noaa.gov/products/noaa-planetary-k-index.json#"
+    );
+    let data = await res.json();
+    // console.log(data);
+    data.map((item) =>
+      // cosnt DateVal =  DateTime.push((item)[0])
+      indexVal.push(item[1])
+    );
+    indexVal.shift();
+    console.log({ indexVal });
+    const ctx = document.getElementById("myChart");
+
+    data.map((item1) =>
+      // cosnt DateVal =  DateTime.push((item)[0])
+      DateTime.push(item1[0])
+    );
+    DateTime.shift();
+    console.log({ DateTime });
+    for (let i = 0; i < DateTime.length; i++) {
+      const pp = new Date(DateTime[i]);
+      const kkk = pp.toLocaleDateString("en-us", { month: "short" });
+      const MonthD = pp.getMonth(pp);
+      const DayD = pp.getDate(pp);
+      console.log(MonthD);
+      let hour = pp.getHours();
+      let minS = pp.getMinutes();
+      if (hour < 10) {
+        hour = "0" + hour;
+      }
+      if (minS < 10) {
+        minS = "0" + minS;
+      }
+      const DateDD = kkk + " " + DayD;
+      console.log({ DateDD });
+      const JJ = hour + ":" + minS;
+      TimeArr.push(DateDD + " " + JJ);
     }
+
+    console.log(TimeArr);
+
+    new Chart(ctx, {
+      type: "bar",
+      data: {
+        labels: TimeArr.slice(-15),
+        datasets: [
+          {
+            label: "Kp index",
+            data: indexVal.slice(-15),
+            borderWidth: 2,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
-
-//  console.log({nom}) 
-loadCharacters()
+//  console.log({nom})
+loadCharacters();
