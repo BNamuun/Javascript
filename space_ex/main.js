083f93c75ad18a5192e9e91a25e891376f062898
Namuun <mit.irnitu@gmail.com>
2023-01-07 01:40:35 +0800

    jjj

--- space_ex/main.js
+++ space_ex/main.js
@@ -1,41 +1,45 @@
-indexVal = []
-DateTime = []
+indexVal = [];
+DateTime = [];
 const loadCharacters = async () => {
-    try {
-        const res = await fetch('https://services.swpc.noaa.gov/products/noaa-planetary-k-index.json#')
-        let data = await res.json();
-        // console.log(data);
-        data.map(item =>
-        indexVal.push((item)[1]))
-        indexVal.shift();
-        console.log({indexVal})
-        const ctx = document.getElementById('myChart');
+  try {
+    const res = await fetch(
+      "https://services.swpc.noaa.gov/products/noaa-planetary-k-index.json#"
+    );
+    let data = await res.json();
+    // console.log(data);
+    data.map((item) => indexVal.push(item[1]));
+    indexVal.shift();
+    console.log({ indexVal });
+    const ctx = document.getElementById("myChart");
 
-        new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: indexVal,
-                datasets: [{
-                    label: '# of Votes',
-                    data: indexVal,
-                    borderWidth: 2
-                }]
+    new Chart(ctx, {
+      type: "bar",
+      data: {
+        labels: indexVal,
+        datasets: [
+          {
+            label: "# of Votes",
+            data: indexVal,
+            borderWidth: 2,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+            grid: {
+              color: (context) => {
+                console.log(context.tick.value);
+              },
             },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
-                }
-            }
-        });
-
-    } catch (err) {
-        console.log(err);
-    }
+          },
+        },
+      },
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
-
-
-
-loadCharacters()
+loadCharacters();
